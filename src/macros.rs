0326bed5b979b0dd4bf0928972b30691d29c3988
keplerHaloxx <christian1soro@gmail.com>
2024-10-03 15:23:06 +1000

    added better printing

---
+++ src/macros.rs
@@ -0,0 +1,48 @@
+#[macro_export]
+macro_rules! styled_vec_print {
+    ($msg:expr, $styles:expr) => {{
+        for style in $styles.iter() {
+            print!("{}", style);
+        }
+        println!("{}{color_reset}{style_reset}", $msg);
+    }};
+
+    ($msg:expr) => {{
+        println!("{}", $msg);
+    }};
+}
+
+#[macro_export]
+macro_rules! styled_print {
+    ($msg:expr, $($style:expr),*) => {
+        {
+            $( print!("{}", $style); )*
+            print!("{}{color_reset}{style_reset}", $msg);
+        }
+    };
+
+    ($msg:expr) => {
+        {
+            print!("{}", $msg);
+        }
+    };
+}
+#[macro_export]
+macro_rules! styled_println {
+    ($msg:expr, $($style:expr),*) => {
+        {
+            styled_print!($msg, $($style),*);
+            println!();
+            // $( print!("{}", $style); )*
+            // println!("{}{color_reset}{style_reset}", $msg);
+        }
+    };
+
+    ($msg:expr) => {
+        {
+            // println!("{}", $msg);
+            styled_print!($msg);
+            println!();
+        }
+    };
+}
