727197a019f9a60b1df5b7c7fb329f0b4819267c
keplerHaloxx <80098945+keplerHaloxx@users.noreply.github.com>
2025-04-11 23:11:55 +1000

    centre trait to easier centre block contents

---
+++ server_gui/src/widgets/mod.rs
@@ -0,0 +1,14 @@
+use ratatui::{
+    layout::Rect,
+    widgets::{Block, Padding},
+};
+
+// makes it easier to centre content inside a block without having to write out the whole padding struct
+pub trait Centre {
+    fn centre(self, area: Rect) -> Self;
+}
+impl<'a> Centre for Block<'a> {
+    fn centre(self, area: Rect) -> Self {
+        self.padding(Padding::new(0, 0, area.height / 2 - 3, 0))
+    }
+}
